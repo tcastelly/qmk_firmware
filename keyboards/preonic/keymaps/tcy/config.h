8d63d125dd06aa4542c9907e9d9bd739ec19f601
Thomas CASTELLY <thomas@castelly.fr>
2023-03-04 22:55:45 +0100

    preonic decease default TAPPING_TERM

--- keyboards/preonic/keymaps/tcy/config.h
+++ keyboards/preonic/keymaps/tcy/config.h
@@ -46,3 +46,6 @@
 
 /* override number of MIDI tone keycodes (each octave adds 12 keycodes and allocates 12 bytes) */
 //#define MIDI_TONE_KEYCODE_OCTAVES 2
+
+// global time for tap dance
+#define TAPPING_TERM 175
