ea7afd444f4b27b31bcf04a2edda8a4f9859a04f
Thomas CASTELLY <thomas@castelly.fr>
2023-03-13 23:22:11 +0100

    local dokcer build if image does not exist

--- build_my_keyboards_with_docker.sh
+++ build_my_keyboards_with_docker.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-set -exo pipefail
 
 git submodule update --init --recursive
 
-# docker build -t qmkfm/qmk_cli .
+r=`docker image inspect qmkfm/qmk_cli 2>/dev/null`
+
+if [ "$r" == [] ]; then
+    docker build -t qmkfm/qmk_cli .
+fi
+
 docker run -v $PWD:/qmk_firmware -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli qmk compile -kb kbdfans/kbd75rgb -km tcy
 docker run -v $PWD:/qmk_firmware -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli qmk compile -kb preonic/rev3_drop -km tcy
 docker run -v $PWD:/qmk_firmware -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli qmk compile -kb splitkb/aurora/corne -km tcy -e CONVERT_TO=elite_pi
