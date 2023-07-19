35f30820273ceec8c67b03380aa71bf3be4bc325
Thomas CASTELLY <thomas@castelly.fr>
2023-07-19 22:40:27 +0200

    migrate corne-ec - in progress ...

---
+++ build_with_docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# git submodule update --init --recursive
+
+r=`docker image inspect qmkfm/qmk_cli:corne_ec 2>/dev/null`
+
+if [ "$r" == [] ]; then
+    docker build -t qmkfm/qmk_cli:corne_ec .
+fi
+
+docker run \
+    --rm \
+    -v $PWD:/qmk_firmware \
+    -v $PWD/.build:/qmk_firmware/.build\
+    -ti qmkfm/qmk_cli:corne_ec\
+    make sekigon/crkbd_ec:tcy:uf2
