cd1d3fbab1d7e8fe95e2d34ac23c4e03d7db5df0
Thomas CASTELLY <thomas@castelly.fr>
2023-12-07 14:51:00 +0100

    add keyball44 - in progress ...

---
+++ keyboards/keyball/keyball44/keymaps/tcy/config.h
@@ -0,0 +1,54 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2022 @Yowkees
+Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#ifdef RGBLIGHT_ENABLE
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_TWINKLE
+#endif
+
+#define TAP_CODE_DELAY 5
+
+#define TAPPING_TERM 175
+#define TAPPING_TERM_PER_KEY
+
+#define SPLIT_WPM_ENABLE
+#define SPLIT_OLED_ENABLE
+#define SPLIT_ACTIVITY_ENABLE
+#define SPLIT_TRANSPORT_MIRROR
+
+#define POINTING_DEVICE_ROTATION_90
+#define SPLIT_POINTING_ENABLE
+#define POINTING_DEVICE_RIGHT
+#define POINTING_DEVICE_TASK_THROTTLE_MS 8
+
+// TRACK_BALL_MIN_DPI used for scroll only
+#define TRACK_BALL_MIN_DPI 2000
+#define TRACK_BALL_DEFAULT_DPI 16500
+#define TRACK_BALL_MAX_DPI 40000
---
+++ keyboards/keyball/keyball44/keymaps/tcy/keymap.c
@@ -0,0 +1,550 @@
+/*
+Copyright 2022 @Yowkees
+Copyright 2022 MURAOKA Taro (aka KoRoN, @kaoriya)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+
+#include "quantum.h"
+
+#include "tapdance.c"
+#ifdef OLED_ENABLE
+  #include "bongo.h"
+#endif
+
+#include "lib/lib8tion/lib8tion.h"
+
+enum oled_modes {
+  OLED_BONGO,
+  OLED_BONGO_WPM,
+  OLED_BONGO_WPM_LAYOUT,
+  OLED_BONGO_LAYOUT,
+  OLED_DEFAULT,
+  OLED_OFF,
+};
+
+int8_t oled_mode = OLED_BONGO_LAYOUT;
+
+// clang-format off
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+  // keymap for default
+  [0] = LAYOUT_universal(
+    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
+    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_7)  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_INT1  ,
+              KC_LALT,KC_LGUI,LCTL_T(KC_LNG2)     ,LT(1,KC_SPC),LT(3,KC_LNG2),                  KC_BSPC,LT(2,KC_ENT), RCTL_T(KC_LNG2),     KC_RALT  , KC_PSCR
+  ),
+
+  [1] = LAYOUT_universal(
+    _______  ,  KC_F1   , KC_F2    , KC_F3   , KC_F4    , KC_F5    ,                                         KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
+    _______  ,  _______ , _______  , KC_UP   , KC_ENT   , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_F12   ,
+    _______  ,  _______ , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
+                  _______  , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+  ),
+
+  [2] = LAYOUT_universal(
+    _______  ,S(KC_QUOT), KC_7     , KC_8    , KC_9     , S(KC_8)  ,                                         S(KC_9)  , S(KC_1)  , S(KC_6)  , KC_LBRC  , S(KC_4)  , _______  ,
+    _______  ,S(KC_SCLN), KC_4     , KC_5    , KC_6     , KC_RBRC  ,                                         KC_NUHS  , KC_MINS  , S(KC_EQL), S(KC_3)  , KC_QUOT  , S(KC_2)  ,
+    _______  ,S(KC_MINS), KC_1     , KC_2    , KC_3     ,S(KC_RBRC),                                        S(KC_NUHS),S(KC_INT1), KC_EQL   ,S(KC_LBRC),S(KC_SLSH),S(KC_INT3),
+                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   KC_DEL   , _______  , _______       , _______  , _______
+  ),
+
+  [3] = LAYOUT_universal(
+    RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
+    RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
+    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
+                  QK_BOOT    , KBC_RST  , _______  ,        _______  , _______  ,                   _______  , _______  , _______       , KBC_RST  , QK_BOOT
+  ),
+};
+// clang-format on
+
+/*
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Auto enable scroll mode when the highest layer is 3
+    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    return state;
+}
+
+#ifdef OLED_ENABLE
+
+#    include "lib/oledkit/oledkit.h"
+
+void oledkit_render_info_user(void) {
+    keyball_oled_render_keyinfo();
+    keyball_oled_render_ballinfo();
+    keyball_oled_render_layerinfo();
+}
+#endif
+*/
+
+#ifdef OLED_ENABLE
+static void render_logo(void) {
+    static const char PROGMEM qmk_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8A, 0x8B, 0x8C, 0x8D, 0x8E, 0x8F, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xA0, 0xA1, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xAB, 0xAC, 0xAD, 0xAE, 0xAF, 0xB0, 0xB1, 0xB2, 0xB3, 0xB4,
+        0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF, 0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0x00
+    };
+
+    oled_write_P(qmk_logo, false);
+}
+
+bool oled_task_user(void) {
+    oled_clear();
+    switch (oled_mode) {
+        default:
+        case OLED_DEFAULT:
+            render_logo();
+            break;
+        case OLED_BONGO:
+            draw_bongo(false, false);
+            break;
+        case OLED_BONGO_WPM:
+            draw_bongo(true, false);
+            break;
+        case OLED_BONGO_LAYOUT:
+            draw_bongo(false, true);
+            break;
+        case OLED_BONGO_WPM_LAYOUT:
+            draw_bongo(true, true);
+            break;
+        case OLED_OFF:
+            oled_off();
+            break;
+    }
+    return false;
+}
+#endif
+
+#ifdef POINTING_DEVICE_ENABLE
+static uint32_t       last_mouse_activity = 0;
+static report_mouse_t last_mouse_report   = {0};
+static bool           is_scrolling        = false;
+report_mouse_t smooth_mouse_movement(report_mouse_t mouse_report) {
+    // Linear interpolation and ease-in-out
+    static fract8 fract = 0.5;
+    int8_t        x     = 0;
+    int8_t        y     = 0;
+    int8_t        h     = 0;
+    int8_t        v     = 0;
+
+    if (!is_scrolling) {
+        x = ease8InOutApprox(lerp8by8(last_mouse_report.x, mouse_report.x, fract));
+        y = ease8InOutApprox(lerp8by8(last_mouse_report.y, mouse_report.y, fract));
+    } else {
+        h = ease8InOutApprox(lerp8by8(last_mouse_report.x, mouse_report.x, fract));
+        v = ease8InOutApprox(lerp8by8(last_mouse_report.y, mouse_report.y, fract));
+    }
+
+    // update the new smoothed report
+    mouse_report.x = x;
+    mouse_report.y = y;
+    mouse_report.h = h;
+    mouse_report.v = v;
+
+    return mouse_report;
+}
+
+report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
+    if (has_mouse_report_changed(&last_mouse_report, &mouse_report)) {
+        last_mouse_activity = timer_read32();
+        memcpy(&last_mouse_report, &mouse_report, sizeof(mouse_report));
+    }
+
+    return smooth_mouse_movement(mouse_report);
+}
+#endif
+
+// Associate our tap dance key with its functionality
+tap_dance_action_t tap_dance_actions[] = {
+    [TD_ESC] = ACTION_TAP_DANCE_TAP_HOLD_LAYOUT(KC_ESC, _ESC),
+    [TD_ESC_OSX] = ACTION_TAP_DANCE_TAP_HOLD_LAYOUT(KC_ESC, _ESC_OSX),
+    [TD_TAB] = ACTION_TAP_DANCE_TAP_HOLD(KC_TAB, KC_TILD),
+    [TD_O] = ACTION_TAP_DANCE_TAP_HOLD(KC_O, KC_LPRN),
+    [TD_P] = ACTION_TAP_DANCE_TAP_HOLD(KC_P, KC_RPRN),
+    [TD_L] = ACTION_TAP_DANCE_TAP_HOLD(KC_L, KC_LCBR),
+    [TD_SCLN] = ACTION_TAP_DANCE_TAP_HOLD(KC_SCLN, KC_RCBR),
+    [TD_ENT] = ACTION_TAP_DANCE_TAP_HOLD(KC_ENT, KC_LSFT),
+
+    // same tap-dance
+    // enable it for osx and linux
+    [TD_LCTL] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_lctl_finished, td_lctl_reset),
+    [TD_LALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_lalt_finished, td_lalt_reset),
+    [TD_LGUI] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_lgui_finished, td_lgui_reset),
+
+    [TD_BSPC] = ACTION_TAP_DANCE_TAP_HOLD(KC_BSPC, LCTL(KC_BSPC)),
+    [TD_BSPC_OSX] = ACTION_TAP_DANCE_TAP_HOLD(KC_BSPC, LALT(KC_BSPC)),
+
+    [TD_DEL] = ACTION_TAP_DANCE_TAP_HOLD_UNPROTECTED(KC_DEL, LCTL(KC_DEL)),
+    [TD_DEL_OSX] = ACTION_TAP_DANCE_TAP_HOLD_UNPROTECTED(KC_DEL, LALT(KC_DEL)),
+
+    [TD_LEFT] = ACTION_TAP_DANCE_TAP_HOLD_UNPROTECTED(KC_LEFT, LCTL(KC_LEFT)),
+    [TD_LEFT_OSX] = ACTION_TAP_DANCE_TAP_HOLD_UNPROTECTED(KC_LEFT, LALT(KC_LEFT)),
+
+    [TD_RIGHT] = ACTION_TAP_DANCE_TAP_HOLD_UNPROTECTED(KC_RIGHT, LALT(KC_RIGHT)),
+    [TD_RIGHT_OSX] = ACTION_TAP_DANCE_TAP_HOLD_UNPROTECTED(KC_RIGHT, LCTL(KC_RIGHT)),
+
+    [TD_RIGHT_OSX] = ACTION_TAP_DANCE_TAP_HOLD_UNPROTECTED(KC_RIGHT, LALT(KC_RIGHT)),
+    [TD_RIGHT] = ACTION_TAP_DANCE_TAP_HOLD_UNPROTECTED(KC_RIGHT, LCTL(KC_RIGHT)),
+
+    [TD_RALT_OSX] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_ralt_osx_finished, td_ralt_osx_reset),
+    [TD_RALT] = ACTION_TAP_DANCE_FN_ADVANCED(NULL, td_ralt_finished, td_ralt_reset)
+};
+
+// Set a long-ish tapping term for tap-dance keys
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case QK_TAP_DANCE ... QK_TAP_DANCE_MAX:
+            return 275;
+        default:
+            return TAPPING_TERM;
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  tap_dance_action_t *action;
+
+  switch (keycode) {
+    case QWERTY:
+        if (record->event.pressed) {
+            layer_move(_QWERTY);
+        }
+        return false;
+        break;
+
+    case QWERTY_OSX:
+        if (record->event.pressed) {
+            layer_move(_QWERTY_OSX);
+        }
+        return false;
+        break;
+
+    case QWERTY_GAMING:
+        if (record->event.pressed) {
+            layer_move(_QWERTY_GAMING);
+        }
+        return false;
+        break;
+
+    case LOWER:
+      if (record->event.pressed) {
+        is_hold_tapdance_disabled = true;
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+
+        // enable scroll
+        #ifdef POINTING_DEVICE_ENABLE
+          is_scrolling = true;
+          pointing_device_set_cpi(TRACK_BALL_MIN_DPI);
+        #endif
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        is_hold_tapdance_disabled = false;
+
+        #ifdef POINTING_DEVICE_ENABLE
+          // disable scroll
+          if (is_scrolling) {  // check if we were scrolling before and set disable if so
+              is_scrolling = false;
+              pointing_device_set_cpi(TRACK_BALL_DEFAULT_DPI);
+          }
+        #endif
+      }
+      return false;
+      break;
+
+    case RAISE:
+      if (record->event.pressed) {
+        is_hold_tapdance_disabled = true;
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+        is_hold_tapdance_disabled = false;
+      }
+      return false;
+      break;
+
+    case KC_LALT:
+    case KC_LGUI:
+    case KC_LSFT:
+      #ifdef POINTING_DEVICE_ENABLE
+        if (record->event.pressed) {
+            pointing_device_set_cpi(TRACK_BALL_MAX_DPI);
+            is_hold_tapdance_disabled = true;
+        } else {
+            pointing_device_set_cpi(TRACK_BALL_DEFAULT_DPI);
+            is_hold_tapdance_disabled = false;
+        }
+      #endif
+      return true;
+      break;
+
+    case ACCENT_CIRCUM:
+      if (record->event.pressed) {
+          register_code(KC_RALT);
+          register_code(KC_6);
+      } else {
+          unregister_code(KC_6);
+          unregister_code(KC_RALT);
+      }
+      break;
+
+    case ACCENT_TREMA:
+      if (record->event.pressed) {
+          register_code(KC_RALT);
+          register_code(KC_LSFT);
+          register_code(KC_QUOT);
+      } else {
+          unregister_code(KC_QUOT);
+          unregister_code(KC_LSFT);
+          unregister_code(KC_RALT);
+      }
+      break;
+
+    case ACCENT_GRAVE:
+      if (record->event.pressed) {
+          register_code(KC_RALT);
+          register_code(KC_GRV);
+      } else {
+          unregister_code(KC_GRV);
+          unregister_code(KC_RALT);
+      }
+      break;
+
+    case ACCENT_E_GRAVE:
+      if (record->event.pressed) {
+          register_code(KC_RALT);
+          register_code(KC_GRV);
+      } else {
+          unregister_code(KC_GRV);
+          unregister_code(KC_RALT);
+          register_code(KC_E);
+          unregister_code(KC_E);
+      }
+      break;
+
+      // to be used with RALT already pressed
+     case ACCENT_A_GRAVE_RALT:
+       if (record->event.pressed) {
+           register_code(KC_GRV);
+       } else {
+           unregister_code(KC_GRV);
+           unregister_code(KC_RALT);
+           register_code(KC_A);
+           unregister_code(KC_A);
+
+           // will be unregister by `td_ralt_reset`
+           register_code(KC_RALT);
+       }
+       break;
+
+     case ACCENT_I_CIRC_RALT:
+       if (record->event.pressed) {
+           register_code(KC_6);
+       } else {
+           unregister_code(KC_6);
+           unregister_code(KC_RALT);
+           register_code(KC_I);
+           unregister_code(KC_I);
+
+           // will be unregister by `td_ralt_reset`
+           register_code(KC_RALT);
+       }
+       break;
+
+     case ACCENT_O_CIRC_RALT:
+       if (record->event.pressed) {
+           register_code(KC_6);
+       } else {
+           unregister_code(KC_6);
+           unregister_code(KC_RALT);
+           register_code(KC_O);
+           unregister_code(KC_O);
+
+           // will be unregister by `td_ralt_reset`
+           register_code(KC_RALT);
+       }
+       break;
+
+     case ACCENT_U_AIGU_RALT:
+       if (record->event.pressed) {
+           register_code(KC_GRV);
+       } else {
+           unregister_code(KC_GRV);
+           unregister_code(KC_RALT);
+           register_code(KC_U);
+           unregister_code(KC_U);
+
+           // will be unregister by `td_ralt_reset`
+           register_code(KC_RALT);
+       }
+       break;
+
+     case ACCENT_C_RALT:
+       if (record->event.pressed) {
+           register_code(KC_COMM);
+       } else {
+           unregister_code(KC_COMM);
+           unregister_code(KC_RALT);
+
+           // will be unregister by `td_ralt_reset`
+           register_code(KC_RALT);
+       }
+       break;
+
+     case ACCENT_A_GRAVE:
+       if (record->event.pressed) {
+           register_code(KC_RALT);
+           register_code(KC_GRV);
+       } else {
+           unregister_code(KC_GRV);
+           unregister_code(KC_RALT);
+           register_code(KC_A);
+           unregister_code(KC_A);
+       }
+       break;
+
+     case JET_RNM:
+       if (record->event.pressed) {
+           register_code(KC_LSFT);
+           register_code(KC_F6);
+
+           unregister_code(KC_LSFT);
+           unregister_code(KC_F6);
+       }
+       return false;
+       break;
+
+     case JET_FIND:
+       if (record->event.pressed) {
+           register_code(KC_LALT);
+           register_code(KC_F1);
+
+           unregister_code(KC_F1);
+           unregister_code(KC_LALT);
+           tap_code(KC_1);
+       }
+       return false;
+       break;
+
+     case TOGGLE_OLED:
+       if (record->event.pressed) {
+           if (oled_mode != OLED_OFF) {
+               oled_mode = OLED_OFF;
+           } else {
+               oled_mode = OLED_BONGO_LAYOUT;
+           }
+       }
+       return false;
+       break;
+
+     case TOGGLE_OLED_DEFAULT:
+       if (record->event.pressed) {
+           if (oled_mode != OLED_DEFAULT) {
+               oled_mode = OLED_DEFAULT;
+           } else {
+               oled_mode = OLED_BONGO_LAYOUT;
+           }
+       }
+       return false;
+       break;
+
+     case TOGGLE_OLED_WPM:
+       if (record->event.pressed) {
+           if (oled_mode == OLED_BONGO) {
+               // add wpm
+               oled_mode = OLED_BONGO_WPM;
+           } else if (oled_mode == OLED_BONGO_WPM) {
+               // remove wpm
+               oled_mode = OLED_BONGO;
+
+           } else if (oled_mode == OLED_BONGO_LAYOUT) {
+               // add wpm
+               oled_mode = OLED_BONGO_WPM_LAYOUT;
+           } else if (oled_mode == OLED_BONGO_WPM_LAYOUT) {
+               // remove wpm
+               oled_mode = OLED_BONGO_LAYOUT;
+           }
+       }
+       return false;
+       break;
+
+     case TOGGLE_OLED_LAYOUT:
+       if (record->event.pressed) {
+           if (oled_mode == OLED_BONGO) {
+               // add layout
+               oled_mode = OLED_BONGO_LAYOUT;
+           } else if (oled_mode == OLED_BONGO_LAYOUT) {
+               // remove layout
+               oled_mode = OLED_BONGO;
+
+           } else if (oled_mode == OLED_BONGO_WPM) {
+               // add layout
+               oled_mode = OLED_BONGO_WPM_LAYOUT;
+           } else if (oled_mode == OLED_BONGO_WPM_LAYOUT) {
+               // remove layout
+               oled_mode = OLED_BONGO_WPM;
+           }
+       }
+       return false;
+       break;
+
+    case TD(TD_O):  // list all tap dance keycodes with tap-hold configurations
+    case TD(TD_ESC):
+    case TD(TD_ESC_OSX):
+    case TD(TD_TAB):
+    case TD(TD_P):
+    case TD(TD_L):
+    case TD(TD_ENT):
+    case TD(TD_SCLN):
+    case TD(TD_BSPC):
+    case TD(TD_BSPC_OSX):
+    case TD(TD_DEL):
+    case TD(TD_DEL_OSX):
+    case TD(TD_LEFT):
+    case TD(TD_LEFT_OSX):
+    case TD(TD_RIGHT):
+    case TD(TD_RIGHT_OSX):
+      if ((keycode == TD(TD_ESC) || keycode == TD(TD_ESC_OSX)) && !record->event.pressed) {
+          layer_off(_ESC);
+          layer_off(_ESC_OSX);
+          is_hold_tapdance_disabled = false;
+
+          #ifdef POINTING_DEVICE_ENABLE
+            pointing_device_set_cpi(TRACK_BALL_DEFAULT_DPI);
+          #endif
+      }
+
+      action = &tap_dance_actions[TD_INDEX(keycode)];
+      if (!record->event.pressed && action->state.count && !action->state.finished) {
+          tap_dance_tap_hold_t *tap_hold = (tap_dance_tap_hold_t *)action->user_data;
+          tap_code16(tap_hold->tap);
+      }
+      break;
+  }
+  return true;
+}
+
+#ifdef OLED_ENABLE
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
+    }
+
+    return rotation;
+}
+#endif
