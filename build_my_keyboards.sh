2e41b82f3d56bde34da6978dd31a3ffaa29a0fee
Thomas CASTELLY <thomas@castelly.fr>
2023-12-08 08:17:32 +0100

    add keyball44 build to docker and native qmk script

--- build_my_keyboards.sh
+++ build_my_keyboards.sh
@@ -4,3 +4,4 @@ qmk compile -kb kbdfans/kbd75rgb -km tcy
 qmk compile -kb preonic/rev3_drop -km tcy
 qmk compile -kb splitkb/aurora/corne -km tcy -e CONVERT_TO=elite_pi
 qmk compile -kb crkbd -km tcy
+qmk compile -kb keyball/keyball44 -km tcy
--- build_my_keyboards_with_docker.sh
+++ build_my_keyboards_with_docker.sh
@@ -12,3 +12,4 @@ docker run -v $PWD:/qmk_firmware --rm -v $PWD/.build:/qmk_firmware/.build -it qm
 docker run -v $PWD:/qmk_firmware --rm -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli qmk compile -kb preonic/rev3_drop -km tcy
 docker run -v $PWD:/qmk_firmware --rm -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli qmk compile -kb splitkb/aurora/corne -km tcy -e CONVERT_TO=elite_pi
 docker run -v $PWD:/qmk_firmware --rm -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli qmk compile -kb crkbd -km tcy
+docker run -v $PWD:/qmk_firmware --rm -v $PWD/.build:/qmk_firmware/.build -it qmkfm/qmk_cli qmk compile -kb keyball/keyball44 -km tcy
