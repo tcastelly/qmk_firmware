bc6fe55f454267ac1cd285cf860ff7cdb969448c
Thomas CASTELLY <thomas@castelly.fr>
2024-02-13 15:32:58 +0100

    add totem keyboard

--- build_my_keyboards.sh
+++ build_my_keyboards.sh
@@ -5,3 +5,4 @@ qmk compile -kb preonic/rev3_drop -km tcy
 qmk compile -kb splitkb/aurora/corne -km tcy -e CONVERT_TO=elite_pi
 qmk compile -kb crkbd -km tcy
 qmk compile -kb keyball/keyball44 -km tcy
+qmk compile -kb totem -km tcy
