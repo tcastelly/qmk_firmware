03bdca4270d805cfd1bf56d69a72eb5abf56b306
Thomas CASTELLY <thomas@castelly.fr>
2024-02-23 17:21:06 +0100

    totem - tcy add EE_HANDS

--- keyboards/totem/keymaps/tcy/config.h
+++ keyboards/totem/keymaps/tcy/config.h
@@ -5,8 +5,9 @@ see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
 for more options.
 */
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
+#define EE_HANDS
 
 #define TAPPING_TERM 170
 #define TAPPING_TERM_PER_KEY
