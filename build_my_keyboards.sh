a4b10e8f84f782603ee15e787252ffd69e457bb0
Thomas CASTELLY <thomas@castelly.fr>
2024-04-19 12:16:54 +0200

    add keyboards to build script

--- build_my_keyboards.sh
+++ build_my_keyboards.sh
@@ -3,9 +3,9 @@
 qmk compile -kb kbdfans/kbd75rgb -km tcy
 qmk compile -kb preonic/rev3_drop -km tcy
 qmk compile -kb splitkb/aurora/corne -km tcy_trackpads
-qmk compile -kb splitkb/aurora/corne -km tcy_trackpoint
-qmk compile -kb splitkb/aurora/corne -km tcy_promorini_trackball_oled
+qmk compile -kb splitkb/aurora/corne -km tcy_pimoroni_trackball_oled
 qmk compile -kb crkbd -km tcy
 qmk compile -kb keyball/keyball44 -km tcy
 qmk compile -kb totem -km tcy
 qmk compile -kb nullbitsco/tidbit -km oled
+qmk compile -kb mc2s -km default -km tcy
