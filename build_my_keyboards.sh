6c8884ff464425a1fd34dde5eb279a90e1423c6a
Thomas CASTELLY <thomas@castelly.fr>
2024-04-19 15:22:08 +0200

    refactore mc2s to abitkeys/mc2s_lp

--- build_my_keyboards.sh
+++ build_my_keyboards.sh
@@ -8,4 +8,4 @@ qmk compile -kb crkbd -km tcy
 qmk compile -kb keyball/keyball44 -km tcy
 qmk compile -kb totem -km tcy
 qmk compile -kb nullbitsco/tidbit -km oled
-qmk compile -kb mc2s -km default -km tcy
+qmk compile -kb abitkeys/mc2s_lp -km tcy
--- keyboards/mc2s/rules.mk
+++
@@ -1,1 +0,0 @@
-DEFAULT_FOLDER = mc2s/rev1
