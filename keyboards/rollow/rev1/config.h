830af6f939cfc896e45f0d1894bbec31610fb01e
Thomas CASTELLY <thomas@castelly.fr>
2024-05-07 11:28:54 +0200

    rollow - add trackpoint, in progress ...

--- keyboards/rollow/rev1/config.h
+++ keyboards/rollow/rev1/config.h
@@ -5,3 +5,15 @@
 /* communication between sides */
 #define USE_SERIAL
 #define SERIAL_USE_MULTI_TRANSACTION
+
+/* track point */
+#define PS1_MOUSE_SCROLL_BTN_MASK (1<<PS2_MOUSE_BTN_MIDDLE) /* Default */
+
+// The default streaming mode is flaky and doesn't always work. This setting doesn't seem to affect performance.
+#define PS2_MOUSE_USE_REMOTE_MODE
+
+// Serial uses PIO0, change PS2 to PIO1.
+#define PS2_PIO_USE_PIO1
+
+// Start faster when keyboard resets.
+#define PS2_MOUSE_INIT_DELAY 500
