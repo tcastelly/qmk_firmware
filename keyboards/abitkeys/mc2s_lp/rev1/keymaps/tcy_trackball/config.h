ba8387ca0bb8d9f2205f9a235d710094082a56fe
Thomas CASTELLY <thomas@castelly.fr>
2024-06-04 15:05:06 +0200

    mc2s_lp - add trackball

---
+++ keyboards/abitkeys/mc2s_lp/rev1/keymaps/tcy_trackball/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+/* place override params */
+#define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+/* AZ1UBALL settings */
+#define PIMORONI_TRACKBALL_SCALE 1    // default 5
---
+++ keyboards/abitkeys/mc2s_lp/rev1/keymaps/tcy_trackball/rules.mk
@@ -0,0 +1,4 @@
+VIA_ENABLE = no            # Enable VIA
+TAP_DANCE_ENABLE = yes
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = pimoroni_trackball
