e33e7d12a2f772a352aab913371eb96012dbccf2
Thomas CASTELLY <thomas@castelly.fr>
2025-09-29 08:27:43 +0200

    add tcy tp keyboard

---
+++ keyboards/tcy/tp/config.h
@@ -0,0 +1,56 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define PS2_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the PS2 implementation uses the PIO0 peripheral
+
+#define I2C1_SCL_PIN GP3
+#define I2C1_SDA_PIN GP2
+
+#define AZOTEQ_IQS5XX_TPS65
+#define AZOTEQ_IQS5XX_ROTATION_270
+
+#define AUDIO_PIN GP19
+#define AUDIO_PWM_DRIVER PWMD1
+#define AUDIO_PWM_CHANNEL RP2040_PWM_CHANNEL_B
+
+#define AUDIO_VOICES
+#define AUDIO_DAC_SAMPLE_MAX 4095U
+#define AUDIO_VOICE_DEFAULT butts_fader
+
+#ifdef AUDIO_ENABLE
+  #define STARTUP_SOUND E__NOTE(_E6), E__NOTE(_A6), ED_NOTE(_E7),
+#endif
+
+// defines for tuning the OLED
+#define OLED_TIMEOUT 15000
+#define OLED_UPDATE_INTERVAL 100
+#define OLED_FADE_OUT
+#define OLED_FADE_OUT_INTERVAL 2
+#define OLED_UPDATE_PROCESS_LIMIT 4
+
+#define PS2_MOUSE_X_MULTIPLIER 1
+#define PS2_MOUSE_Y_MULTIPLIER 1
+#define PS2_MOUSE_V_MULTIPLIER 2
---
+++ keyboards/tcy/tp/keymaps/tcy/config.h
@@ -0,0 +1,75 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+// defines for making the Trackpoint more fluid
+#define PS2_MOUSE_USE_REMOTE_MODE
+
+#define PS2_MOUSE_SCROLL_BTN_SEND 500 /* Default is 300 */
+
+#undef PS2_MOUSE_SCROLL_BTN_MASK
+#define PS2_MOUSE_SCROLL_BTN_MASK (1<<PS2_MOUSE_BTN_MIDDLE) /* Default */
+#define PS2_MOUSE_BTN_LEFT      0
+#define PS2_MOUSE_BTN_RIGHT     1
+#define PS2_MOUSE_BTN_MIDDLE    2
+
+#define PS2_MOUSE_INIT_DELAY 1000 /* Default */
+
+#ifdef PS2_USE_USART
+/* synchronous, odd parity, 1-bit stop, 8-bit data, sample at falling edge */
+/* set DDR of CLOCK as input to be slave */
+#define PS2_USART_INIT() do {   \
+	PS2_CLOCK_DDR &= ~(1<<PS2_CLOCK_BIT);   \
+	PS2_DATA_DDR &= ~(1<<PS2_DATA_BIT);     \
+	UCSR1C = ((1 << UMSEL10) |  \
+			(3 << UPM10)   |  \
+			(0 << USBS1)   |  \
+			(3 << UCSZ10)  |  \
+			(0 << UCPOL1));   \
+	UCSR1A = 0;                 \
+	UBRR1H = 0;                 \
+	UBRR1L = 0;                 \
+} while (0)
+#define PS2_USART_RX_INT_ON() do {  \
+	UCSR1B = ((1 << RXCIE1) |       \
+			(1 << RXEN1));        \
+} while (0)
+#define PS2_USART_RX_POLL_ON() do { \
+	UCSR1B = (1 << RXEN1);          \
+} while (0)
+#define PS2_USART_OFF() do {    \
+	UCSR1C = 0;                 \
+	UCSR1B &= ~((1 << RXEN1) |  \
+			(1 << TXEN1));  \
+} while (0)
+#define PS2_USART_RX_READY      (UCSR1A & (1<<RXC1))
+#define PS2_USART_RX_DATA       UDR1
+#define PS2_USART_ERROR         (UCSR1A & ((1<<FE1) | (1<<DOR1) | (1<<UPE1)))
+#define PS2_USART_RX_VECT       USART1_RX_vect
+#endif
+
+#define MIN_ACCELERATION_SETTING         1;
+#define MAX_ACCELERATION_SETTING         6;
+#define DEFAULT_ACCELERATION_SETTING     5;
