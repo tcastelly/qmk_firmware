f0a4789433eae722aa34667190cc0685463c45a3
Thomas CASTELLY <thomas@castelly.fr>
2025-12-21 11:34:24 +0100

    add tcy - tcy_trackball

---
+++ keyboards/splitkb/aurora/corne/keymaps/tcy_trackball/config.h
@@ -0,0 +1,50 @@
+/*
+Copyright 2022 x123 <@x123>
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define TAPPING_TERM 175
+#define TAPPING_TERM_PER_KEY
+
+// use the power light of liatris controller
+// to display the cap lock light
+// #define LED_CAPS_LOCK_PIN 24
+// #define LED_PIN_ON_STATE 0
+
+#define SPLIT_ACTIVITY_ENABLE
+#define SPLIT_TRANSPORT_MIRROR
+
+// #define MASTER_RIGHT
+
+#define SPLIT_TRANSPORT_MIRROR
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_MODS_ENABLE
+
+// #define RGBLIGHT_LAYERS
+// #define RGBLIGHT_LAYER_BLINK
+// #undef RGBLIGHT_DEFAULT_MODE
+// #define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_STATIC_LIGHT
+
+#ifdef RGBLIGHT_ENABLE
+#   define RGBLIGHT_EFFECT_BREATHING
+#endif
+
+#define SPI_DRIVER SPID1
+#define POINTING_DEVICE_INVERT_X
+#define SPI_SCK_PIN GP14
+#define SPI_MOSI_PIN GP15
+#define SPI_MISO_PIN GP12
+#define PMW33XX_CS_PIN GP13
