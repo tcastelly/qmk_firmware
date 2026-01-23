7224da2ff309d7a80079f12b4bf8824a23ac6279
Thomas CASTELLY <thomas@castelly.fr>
2026-01-23 20:47:38 +0100

    tcy noam_hatsu

--- keyboards/tcy/noam_hatsu/config.h
+++ keyboards/tcy/noam_hatsu/config.h
@@ -1,9 +1,12 @@
 #pragma once
 
-#define MATRIX_DEBUG
-
 // Auto-detect handedness using GP3
-#define SPLIT_HAND_PIN GP3
+// #define SPLIT_HAND_PIN GP3
+// #define SPLIT_HAND_PIN_LOW_IS_LEFT
+// #define SPLIT_USB_DETECT
+//
+// #define EE_HANDS
+#define MASTER_RIGHT
 
 // Left (slave) half matrix
 #define MATRIX_COL_PINS { GP21, GP23, GP20, GP22, GP26, GP27, GP28, GP29 }
@@ -12,5 +15,3 @@
 // Right (master) half matrix
 #define MATRIX_COL_PINS_RIGHT { GP21, GP23, GP20, GP22, GP26, GP27, GP28, GP29 }
 #define MATRIX_ROW_PINS_RIGHT { GP4, GP5, GP6, GP7 }
-
-#define EE_HANDS
--- keyboards/tcy/noam_hatsu/keymaps/test/keymap.c
+++ keyboards/tcy/noam_hatsu/keymaps/test/keymap.c
@@ -5,9 +5,9 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_split_3x8_2(
-        KC_0,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,         KC_A,     KC_B,    KC_C,    KC_D,    KC_E, KC_F,   KC_G,    KC_H,
+        KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,     KC_7,         QK_BOOT,  KC_B,    KC_C,    KC_D,    KC_E, KC_F,   KC_G,    KC_H,
         KC_8,    KC_9,    KC_ENT,  KC_ESC,  KC_BSPC, KC_SPC,  KC_MINS,  KC_EQL,       KC_I,     KC_J,    KC_K,    KC_L,    KC_M, KC_N,   KC_O,    KC_P,
-        KC_LBRC, KC_RBRC, KC_BSLS, KC_SCLN, KC_QUOT, KC_GRV,  KC_COMM,  KC_SLSH,      KC_Q,     KC_R,    KC_S,    KC_T,    KC_U, KC_V,   KC_W,    KC_X,
+        QK_BOOT, KC_RBRC, KC_BSLS, KC_SCLN, KC_QUOT, KC_GRV,  KC_COMM,  KC_SLSH,      KC_Q,     KC_R,    KC_S,    KC_T,    KC_U, KC_V,   KC_W,    KC_X,
                                             KC_F1,   KC_F2,                                                                KC_Y, KC_Z
     )
 };
@@ -19,5 +19,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //     KC_H,  KC_G,    KC_F,    KC_E,    KC_D,    KC_C,                         KC_BSLS, KC_SCLN, KC_QUOT, KC_GRV,  KC_SLSH, KC_COMM,
 //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 //     KC_EQL, KC_MINS, KC_SPC, KC_BSPC, KC_ESC,  KC_ENT,                       KC_S,    KC_T,    KC_U,    KC_V,    KC_X,    KC_W,
+//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+//                      KC_F2,  KC_F1                                                             KC_Y,   KC_Z
 //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 //                                         KC_0, KC_9, KC_B, KC_1,   KC_J, KC_RBRC, KC_R, KC_I,
+//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+//|                                               QK_BOOT, KC_DEL,   KC_DEL, QK_BOOT
+
