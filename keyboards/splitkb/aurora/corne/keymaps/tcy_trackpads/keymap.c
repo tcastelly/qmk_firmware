7d87ff867b5b38c8318625a0dbd1eac45ed59a28
Thomas CASTELLY <thomas@castelly.fr>
2026-06-26 14:31:49 +0200

    tcy - splitkb, change static orange color to "fire" effect

--- keyboards/splitkb/aurora/corne/keymaps/tcy_trackpads/keymap.c
+++ keyboards/splitkb/aurora/corne/keymaps/tcy_trackpads/keymap.c
@@ -3,12 +3,63 @@
 #include "../tcy/keymaps.c"
 #include "tcy.h"
 
-//Lighting
+// --- Fire effect --------------------------------------------------------
+#define FIRE_UPDATE_MS 50
+
+static uint16_t fire_timer  = 0;
+static bool     fire_inited = false;
+static uint8_t  fire_heat[RGB_MATRIX_LED_COUNT];
+static uint16_t fire_lfsr   = 0x1337;
+
+static uint8_t fire_rand8(void) {
+    fire_lfsr ^= fire_lfsr << 7;
+    fire_lfsr ^= fire_lfsr >> 9;
+    fire_lfsr ^= fire_lfsr << 8;
+    return (uint8_t)(fire_lfsr ^ (fire_lfsr >> 8));
+}
+
+// heat 0=deep red, 128=orange, 255=bright yellow-orange
+static void heat_to_rgb(uint8_t heat, uint8_t *r, uint8_t *g, uint8_t *b) {
+    if (heat < 128) {
+        *r = 128 + heat;
+        *g = heat / 2;
+        *b = 0;
+    } else {
+        *r = 255;
+        *g = 64 + (heat - 128);
+        *b = (heat - 128) / 8;
+    }
+}
+
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (keep_rgb_off) return false;
 
+    if (!fire_inited) {
+        for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+            fire_heat[i] = 80 + fire_rand8() % 170;
+        }
+        fire_inited = true;
+        fire_timer  = timer_read();
+    }
+
+    if (timer_elapsed(fire_timer) >= FIRE_UPDATE_MS) {
+        fire_timer = timer_read();
+        for (uint8_t i = 0; i < RGB_MATRIX_LED_COUNT; i++) {
+            int16_t h = (int16_t)fire_heat[i] + (int16_t)(fire_rand8() % 41) - 20;
+            if (h < 60)  h = 60  + (fire_rand8() & 0x0F);
+            if (h > 250) h = 250 - (fire_rand8() & 0x0F);
+            fire_heat[i] = (uint8_t)h;
+        }
+    }
+
+    uint8_t val = rgb_matrix_get_val();
     for (uint8_t i = led_min; i < led_max; i++) {
-      rgb_matrix_set_color(i, COLOR_ORANGE[0], COLOR_ORANGE[1], COLOR_ORANGE[2]);
+        uint8_t r, g, b;
+        heat_to_rgb(fire_heat[i], &r, &g, &b);
+        r = (uint8_t)((uint16_t)r * val / 255);
+        g = (uint8_t)((uint16_t)g * val / 255);
+        b = (uint8_t)((uint16_t)b * val / 255);
+        rgb_matrix_set_color(i, r, g, b);
     }
 
     return false;
